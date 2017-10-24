656c562dafac4a92e8ef3e6ceba40b3816dbbcb7
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-10-24 18:03:39 +0800

    add sticky footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,30 +6,61 @@ import Helmet from 'react-helmet';
 import './index.scss';
 
 const Header = () => (
-  <div >
-    <div>
-      <h1>
-        <Link to="/">
-          Gatsby
+  <nav className="navbar is-primary" role="navigation" aria-label="main-navigation">
+    <div className="container">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <span className="title has-text-white">CTN</span>
         </Link>
-      </h1>
+        <button className="button is-primary navbar-burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/page-2/">
+            About
+          </Link>
+          <Link className="navbar-item" to="/page-2/">
+            Services
+          </Link>
+          <Link className="navbar-item" to="/page-2/">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  </nav>
+);
+
+const Footer = () => (
+  <div className="footer">
+    <div className="container">
+      <div className="content has-text-centered">
+        <p>
+          footer text
+        </p>
+      </div>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="site">
     <Helmet
-      title="Gatsby Default Starter"
+      title="Controlnet"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' }
       ]}
     />
     <Header />
-    <div>
+    <div className="wrapper">
       {children()}
     </div>
+    <Footer />
   </div>
 );
 

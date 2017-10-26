fbd519bbce75b84297310f65a31a01944993bfe8
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-10-26 14:52:50 +0800

    make logo bigger

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
@@ -5,42 +6,69 @@ import Helmet from 'react-helmet';
 
 import './index.scss';
 
-const Header = () => (
-  <nav className="navbar is-primary" role="navigation" aria-label="main-navigation">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link className="navbar-item" to="/">
-          <span className="title has-text-white">CTN</span>
-        </Link>
-        <button className="button is-primary navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </div>
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <Link className="navbar-item" to="/page-2/">
-            About
-          </Link>
-          <Link className="navbar-item" to="/page-2/">
-            Services
-          </Link>
-          <Link className="navbar-item" to="/page-2/">
-            Contact
-          </Link>
+class Header extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      menu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu () {
+    let menu = this.state.menu;
+    let el = document.querySelector('.wrapper');
+    let handler = () => this.setState({menu: false});
+    if (!menu) {
+      this.setState({menu: true});
+      el.addEventListener('click', handler);
+    } else {
+      this.setState({menu: false});
+      el.removeEventListener('click', handler);
+    }
+  }
+
+  render () {
+    return (
+      <nav className="navbar" role="navigation" aria-label="main-navigation">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              <img src={require('../assets/img/ctn-FONT_003.png')}/>
+            </Link>
+            <a className={'navbar-burger ' + (this.state.menu ? 'is-active' : '')}
+              onClick={this.toggleMenu}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </a>
+          </div>
+          <div className={'navbar-menu ' + (this.state.menu ? 'is-active' : '')}>
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/about/" onClick={this.toggleMenu}>
+                About
+              </Link>
+              <Link className="navbar-item" to="/services/" onClick={this.toggleMenu}>
+                Services
+              </Link>
+              <Link className="navbar-item" to="/contact/" onClick={this.toggleMenu}>
+                Contact
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
+      </nav>
+    );
+  }
+}
 
 const Footer = () => (
   <div className="footer">
     <div className="container">
-      <div className="content has-text-centered">
+      <div className="content">
         <p>
-          footer text
+          Controlnet International
         </p>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 //import Link from 'gatsby-link';
+const background = require('../assets/img/reflection-2339624_1280.jpg');
 
 const IndexPage = () => (
   <div>
-    <div className="hero is-info is-medium">
-      <div className="hero-body">
+
+    <div className="hero is-light is-large">
+      <div className="hero-body" style={{background: `url(${background}) no-repeat center bottom`, backgroundSize: 'cover'}}>
         <div className="container">
           <h1 className="title">
-            text
+            Title
           </h1>
         </div>
       </div>
     </div>
+
   </div>
 );
 
